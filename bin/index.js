4ebb19ec1cf3964fab482799ffbba0340465c79a
Hans Magnus Inderberg <hinderberg@gmail.com>
2013-06-01 12:53:38 +0200

    Instead of re-using the same logger, each module can create their own

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,9 @@
 var errors = require('../lib/errors');
 var validation = require('../lib/validation');
+var logger = require('../lib/logger');
 
 module.exports = {
   errors: errors,
-  validation: validation
+  validation: validation,
+  logger: logger
 };
