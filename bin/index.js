b145a4c39e06f6c766223e4d5e5d09b12a89bc34
Hans Magnus Inderberg <hinderberg@gmail.com>
2013-09-19 17:44:25 +0200

    Added a base model and use it on task model

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,12 @@
 var errors = require('../lib/errors');
 var validation = require('../lib/validation');
 var logger = require('../lib/logger');
+var Model = require('../lib/model');
 
 module.exports = {
   errors: errors,
+  Model: Model,
   validation: validation,
   logger: logger
 };
+;
