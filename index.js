af6a3cf220cce87d15ec30c3c542457606ac265b
Hans Magnus Inderberg <hinderberg@gmail.com>
2013-10-20 18:00:58 +0200

    Added a reusable websocket component

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var errors = require('./lib/errors');
 var validation = require('./lib/validation');
 var logger = require('./lib/logger');
 var Model = require('./lib/model');
+var ws = require('./lib/ws');
 
 module.exports = {
   errors: errors,
   Model: Model,
   validation: validation,
-  logger: logger
+  logger: logger,
+  ws: ws
 };
