f86a5ca7e9bdead5299455b611e075dfea840e40
Hans Magnus Inderberg <hinderberg@gmail.com>
2013-11-05 17:15:40 +0100

    Removed amd clutter and added versions

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
-(function (factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
-    define(function(require) {
-      factory(require, require('exports'), require('module'))
-    });
-  } else if (typeof exports === 'object') {
-    // CommonJS
-    factory(require, exports, module);
-  }
-}(function (require, exports, module) {
-	
-	var validation = require('./lib/validation');
-	var Model = require('./lib/model');
+var validation = require('./lib/validation');
+var Model = require('./lib/model');
 
-	module.exports = {
-	  Model: Model,
-	  validation: validation
-	};
+module.exports = {
+  Model: Model,
+  validation: validation
+};
 
-}));
